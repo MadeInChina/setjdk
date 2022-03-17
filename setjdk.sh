8474286449ea74a92fcda297d8a1f20a99a2b66e
itshan <slamhan1987@gmail.com>
2022-03-17 12:04:12 +0800

    Create setjdk.sh

---
+++ setjdk.sh
@@ -0,0 +1,33 @@
+# Function to switch JDK versions
+function setjdk() {
+  echo "Select the java version you wish to switch to from the following:"
+  echo
+  allVersions=()
+  count=0
+
+  while IFS= read -r line; do
+    if [[ ! "$line" == M* ]] && [[ ! "$line" == /* ]] && [[ ! -z "$line" ]]; then
+      allVersions+=($line)
+      ((count++))
+      echo '['"$count"']'${allVersions[$count]}
+    fi
+  done < <(/usr/libexec/java_home -V 2>&1)
+
+  echo "Please chose a value from the options above:"
+  read selectedOption </dev/tty
+
+  if [ "$count" -ge "$selectedOption" ] && [ "$selectedOption" -ge '0' ]; then
+    removeFromPath '/System/Library/Frameworks/JavaVM.framework/Home/bin'
+    removeFromPath "$JAVA_HOME/bin"
+    stringArray=(${allVersions[$selectedOption]})
+    export JAVA_HOME="${stringArray[@]: -1}"
+    export PATH="$JAVA_HOME"/bin:$PATH
+    echo "JAVA_HOME is set to be ${JAVA_HOME}"
+  else
+    echo "Invalid option, JAVA_HOME was not set"
+  fi
+}
+
+function removeFromPath() {
+  export PATH=$(echo $PATH | sed -E -e "s;:$1;;" -e "s;$1:?;;")
+}
